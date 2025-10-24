b71340d6fc2672df4096809a00446557142d427c
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2025-10-24 17:50:16 -0300

    ms-user(feat): add tests

---
+++ ms-user/src/test/java/br/com/challenge/payment/core/model/UserTest.java
@@ -0,0 +1,21 @@
+package br.com.challenge.payment.core.model;
+
+import br.com.challenge.payment.helpers.model.UserHelper;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void testShouldBeLojist() {
+        User user = UserHelper.createUserLojist();
+        assertTrue(user.isUserLojist());
+    }
+
+    @Test
+    void testShouldUserCommon() {
+        User user = UserHelper.createUserCommon();
+        assertTrue(user.isUserCommon());
+    }
+}
