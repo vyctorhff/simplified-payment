98d3a2bacd485b32fdf7a004a39a2f2d54ed9c0c
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2025-10-27 10:00:34 -0300

    ms-transference(feat): rename PaymentRequestDTO and remove class not used

--- ms-transference/src/main/java/br/com/challenge/payment/core/validator/UserBalanceValidator.java
+++
@@ -1,9 +0,0 @@
-package br.com.challenge.payment.core.validator;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class UserBalanceValidator {
-}
