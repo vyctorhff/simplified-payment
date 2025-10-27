6d50edd57f1514118ab2ef248a82ae6e31e948a3
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2025-10-27 08:48:05 -0300

    ms-transference(feat): adding validatiors

---
+++ ms-transference/src/main/java/br/com/challenge/payment/core/validator/UserBalanceValidator.java
@@ -0,0 +1,9 @@
+package br.com.challenge.payment.core.validator;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserBalanceValidator {
+}
